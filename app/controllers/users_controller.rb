5e5c801faba821956d054074ed8c9e0f382f3f1d
ayano309 <ayatomato.k@gmail.com>
2022-05-03 10:51:11 +0000

    ユーザ詳細ページに、投稿数を表示させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,20 @@ class UsersController < ApplicationController
     @users = User.all
     @book = Book.new
     @books = Book.all
-    
+
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+
+    #特定の日に保存された投稿数
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    #今週、前週
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def edit
@@ -20,7 +27,7 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user.id)
     end
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -29,7 +36,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name,:introduction,:profile_image)

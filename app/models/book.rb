db628268b0de8e8ac322440306f341325597af30
ayano309 <ayatomato.k@gmail.com>
2022-05-03 14:12:21 +0000

    chart.js導入

--- app/models/book.rb
+++ app/models/book.rb
@@ -32,4 +32,20 @@ class Book < ApplicationRecord
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
   #先週の投稿数
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
+
+
+  #７日間の投稿データ取得
+  # scope :created_2days, -> { where(created_at: 2.days.ago.all_day) } 
+  # scope :created_3days, -> { where(created_at: 3.days.ago.all_day) } 
+  # scope :created_4days, -> { where(created_at: 4.days.ago.all_day) } 
+  # scope :created_5days, -> { where(created_at: 5.days.ago.all_day) } 
+  # scope :created_6days, -> { where(created_at: 6.days.ago.all_day) } 
+  
+  #リファクタリング
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
+
+ #0~6になってしまうので、反転させるためにreverse
+  def self.past_week_count
+    (1..6).map { |n| created_days_ago(n).count }.reverse
+  end
 end
